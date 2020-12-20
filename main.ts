df843c47eaaca2f34fe7819ccb22df9edb22d3e9
jwindleGitHub <75438765+jwindleGitHub@users.noreply.github.com>
2020-12-20 12:20:10 -0500

    Update pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,5 +1,16 @@
+input.onButtonPressed(Button.A, function () {
+    basic.showString("MERRY CHRISTMAS,CARTER")
+    basic.pause(1000)
+})
+input.onButtonPressed(Button.AB, function () {
+    basic.showString("HAPPY NEW YEAR, TOO")
+    basic.pause(1000)
+})
+input.onButtonPressed(Button.B, function () {
+    basic.showString("HAPPY BIRTHDAY, BUBBA")
+    basic.pause(1000)
+})
 basic.showString("Hello!")
 basic.forever(function () {
-    basic.showString("MERRY CHRISTMAS, CARTER!")
-    basic.pause(1000)
+	
 })
